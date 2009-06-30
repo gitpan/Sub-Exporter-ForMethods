963395fd52d118d6d09add1b51a60cfdb6dc7f0e
Ricardo SIGNES <rjbs@cpan.org>
2009-06-30 12:55:49 +0000

    Import of RJBS/Sub-Exporter-ForMethods-0.091810 from CPAN.
    gitpan-cpan-distribution: Sub-Exporter-ForMethods
    gitpan-cpan-version:      0.091810
    gitpan-cpan-path:         RJBS/Sub-Exporter-ForMethods-0.091810.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ t/lib/TestDexp.pm
@@ -0,0 +1,19 @@
+use strict;
+use warnings;
+package TestDexp;
+our $VERSION = '0.091810';
+
+
+use Sub::Exporter -setup => {
+  exports   => [ qw(foo) ],
+};
+
+use Carp ();
+
+sub foo {
+  my ($self, @arg) = @_;
+
+  return Carp::longmess("$self -> foo ( @_ )");
+}
+
+1;
---
+++ t/lib/TestMexp.pm
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+package TestMexp;
+our $VERSION = '0.091810';
+
+
+use Sub::Exporter::ForMethods qw(method_installer);
+use Sub::Exporter -setup => {
+  exports   => [ qw(foo) ],
+  installer => method_installer,
+};
+
+use Carp ();
+
+sub foo {
+  my ($self, @arg) = @_;
+
+  Carp::longmess("$self -> foo ( @_ )");
+}
+
+1;
